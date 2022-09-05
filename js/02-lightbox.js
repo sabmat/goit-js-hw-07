cce573b1da8180ff129c14828bcf5c162d114ec1
sabmat <100289478+sabmat@users.noreply.github.com>
2022-09-05 17:10:06 +0300

    finish work

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -12,7 +12,7 @@ function addImgGallery(galleryItems) {
     return galleryItems
       .map(({ preview, original, description }) => {
         return `<a class="gallery__item" href="${original}">
-        <img class="gallery__image" src="${preview}" alt="${description}" />
+        <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
       </a>`;
       })
       .join("");
